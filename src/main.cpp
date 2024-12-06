69074357edb1a611ea9e59e759b605e33b63fbbe
ariel <arielmw87@gmail.com>
2024-12-06 10:22:25 -0300

    agrego test de motores

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 #include <util/delay.h> //para usar _delay_ms()
 #include <util.h>
-#include <display.h>
 #include <sensores.h>
+#include <motor.h>
 
 void setup(){
 
   config_io();     // configuro las entradas y salidas
+  motor_init();
   config_ultrasonic_measure(DISTANCE_MODE_CONTINUOUS);
   sei();
   Serial.begin(9600);
@@ -15,11 +16,18 @@ void setup(){
 
 
 void loop(){
+
+
   Serial.println(get_s1_centimeters());
   Serial.println(get_s2_centimeters());
   Serial.println(get_s3_centimeters());
   Serial.println("--------------------");
-  delay(300);
+  
+  go(FORWARD,102);
+  delay(150);
+  
+  go(BACKWARD,102);
+  delay(150);
 
 
 }
