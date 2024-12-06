b8ee244e61a1164865a9bc97ec76f704bab1eba7
ariel <arielmw87@gmail.com>
2024-12-06 10:21:31 -0300

    agrego biblioteca manejo motores

--- lib/motor/motor.c
+++ lib/motor/motor.c
@@ -1,37 +1,112 @@
 #include <motor.h>
 
+
 void go(uint8_t direction, uint8_t speed)
 {
-
-    set_speed(0);
+    set_speed(0,0);
 
     switch (direction)
     {
     //---------------------
     case FORWARD:
+        //MOTOR R
+        bit_set(PORTD,PD2);
+        bit_clear(PORTD,PD4);
+        //MOTOR L
+        bit_set(PORTD,PD2);
+        bit_clear(PORTD,PD4);
+
+        set_speed(speed,speed);
         break;
     //---------------------
     case BACKWARD:
+        //MOTOR R
+        bit_clear(PORTD,PD2);
+        bit_set(PORTD,PD4);
+        //MOTOR L
+        bit_clear(PORTD,PD2);
+        bit_set(PORTD,PD4);
+
+        set_speed(speed,speed);
         break;
     //---------------------
     case LEFT:
+        //MOTOR R
+        bit_clear(PORTD,PD2);
+        bit_set(PORTD,PD4);
+        //MOTOR L
+        bit_set(PORTD,PD2);
+        bit_clear(PORTD,PD4);
+        set_speed(speed,speed);
         break;
     //---------------------
     case RIGHT:
+        //MOTOR R
+        bit_set(PORTD,PD2);
+        bit_clear(PORTD,PD4);
+
+        //MOTOR L
+        bit_clear(PORTD,PD2);
+        bit_set(PORTD,PD4);
+
+        set_speed(speed,speed);
         break;
-        //---------------------
+    //---------------------
+    case FRONT_LEFT:
+        //MOTOR R
+        bit_set(PORTD,PD2);
+        bit_clear(PORTD,PD4);
+        //MOTOR L
+        bit_set(PORTD,PD2);
+        bit_clear(PORTD,PD4);
+
+        set_speed(speed/2,speed);
+        break;
+    //---------------------
+    case FRONT_RIGHT:
+        //MOTOR R
+        bit_set(PORTD,PD2);
+        bit_clear(PORTD,PD4);
+        //MOTOR L
+        bit_set(PORTD,PD2);
+        bit_clear(PORTD,PD4);
+
+        set_speed(speed,speed/2);
+        break;
+    //---------------------
 
     default:
-        speed=0;
         break;
     }
 
-    set_speed(speed);
 }
 
-void set_speed(uint8_t speed)
+void set_speed(uint8_t speed_L, uint8_t speed_R)
 {
-
-    //setear PWM
-
+    OCR0A = speed_R;  // 0-255 -> 0-100% de duty cycle para PD6
+    OCR0B = speed_L;  // 0-255 -> 0-100% de duty cycle para PD5
 }
+
+
+
+void motor_init(){
+    // Configurar los pines PD5 y PD6 como salidas
+    DDRD |= (1<<PD2) | (1<<PD3) | (1<<PD4) | (1<<PD5) | (1<<PD6) | (1<<PD7);
+    PORTD &= ~( (1<<PD2) | (1<<PD3) | (1<<PD4) | (1<<PD5) | (1<<PD6) | (1<<PD7) );
+    
+    // Configurar Timer 0 en modo Fast PWM
+    TCCR0A |= (1 << WGM00) | (1 << WGM01);  // Modo Fast PWM
+    TCCR0A |= (1 << COM0A1) | (1 << COM0B1);  // Comparar salida en modo no inverso para OC0A y OC0B
+    TCCR0B |= (1 << CS01) | (1 << CS00);  // Prescaler de 64
+    //frec = 16Mz / ( prescaler * 256 )
+    //frec = 16Mz / (64*256) = 976.56Hz
+
+    // Establecer el duty cycle inicial (50% por ejemplo)
+    OCR0A = 0;  // 0% de duty cycle para PD6
+    OCR0B = 0;  // 0% de duty cycle para PD5
+}
+
+
+
+
+
--- lib/motor/motor.h
+++ lib/motor/motor.h
@@ -5,23 +5,36 @@
 #include <Arduino.h>
 #include <util.h>
 
-#define     FORWARD     1
-#define     BACKWARD    2
-#define     LEFT        3
-#define     RIGHT       4
+//#define     PUSH        1
+#define     FORWARD     2
+#define     BACKWARD    3
+#define     LEFT        4
+#define     RIGHT       5
+#define     FRONT_LEFT  6
+#define     FRONT_RIGHT 7
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+
 /**
- * direction = FORWARD, BACKWARD, LEFT, RIGTH
+ * direction = FORWARD, BACKWARD, LEFT, RIGHT, FRONT_LEFT, FRONT_RIGHT
  * speed = 0 to 255
 */
 void go(uint8_t direction, uint8_t speed);
 
-void set_speed(uint8_t speed);
+/**
+ * Speed 0 - 255
+ */
+void set_speed(uint8_t speed_L,uint8_t speed_R);
+
+
+/**
+ * initialize PWM on pins PD5 and PD6
+ */
+void motor_init();
 
 #ifdef __cplusplus
 }
